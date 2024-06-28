e89af61acd45c5a7bfe737cde7c23eca24109561
Filipe Andre Floriani <filipef@weg.net>
2024-06-28 13:14:41 -0300

    feat: implementado componentização do header

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+import Logo from '../../componentes/Logo'
+import OpcoesHeader from '../../componentes/OpcoesHeader'
+import IconesHeader from '../../componentes/IconesHeader';
+
+function Header() {
+    return (  
+        <header className='App-header'>
+            <Logo />
+            <OpcoesHeader />
+            <IconesHeader />
+        </header>      
+    )
+}
+
+export default Header
