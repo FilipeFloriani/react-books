e11b639275b157cd8d69ee0dddac3d3421c4bcfc
Filipe Andre Floriani <filipef@weg.net>
2024-07-02 15:58:59 -0300

    feat: inicio implementação da pesquisa

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,13 @@
+import Input from "../Input"
+
+function Pesquisa() {
+    return (
+        <section>
+            <h2>Já sabe por onde começar?</h2>
+            <h3>Encontre o que você procura</h3>
+            <Input />
+        </section>
+    )
+}
+
+export default Pesquisa
